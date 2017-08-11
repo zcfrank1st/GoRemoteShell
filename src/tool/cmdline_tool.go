8e4a504778d60999e4f3df2775f956447304d31a
zcfrank1st <zcfrank1st@sina.com>
2017-08-11 09:52:26 +0800

    fix code

--- src/tool/cmdline_tool.go
+++ src/tool/cmdline_tool.go
@@ -10,23 +10,32 @@ import (
     "github.com/lukesampson/figlet/figletlib"
     "flag"
     "github.com/logrusorgru/aurora"
+    "io/ioutil"
 )
 
 type colorFunc func(interface{}) aurora.Value
 
-var (
-    SHELL_NAME = "ShootMan"
+const (
+    SHELL_NAME    = "ShootMan"
     SHELL_VERSION = "v1.0"
-    INNER_COMMANDS = map[string]int {"session": 1, "help": 2, "clean": 3, "leave": 4}
+
+    LINE_SEP      = "\n"
+    FIELD_SEP     = "\t"
+)
+
+var (
+    INNER_COMMANDS = map[string]int {"session": 1, "help": 2, "clean": 3, "leave": 4, "save": 5}
 
     session = map[string]string{}
     current_session string
 
     fontPath string
+    sessionFile string
 )
 
 func init () {
     flag.StringVar(&fontPath, "font-path", "", "set fonts path")
+    flag.StringVar(&sessionFile, "session-file", "/tmp/shootman.ini", "set store session file path")
     flag.Parse()
 }
 
@@ -34,13 +43,14 @@ func main() {
     if fontPath != "" {
         f, _ := figletlib.GetFontByName(fontPath, "big")
         PrintMsgWithColor(SHELL_NAME, f, 80, f.Settings(), "left", aurora.Cyan)
-        fmt.Printf("%50s", aurora.Cyan(SHELL_VERSION + "\n\n"))
+        fmt.Printf("%50s", aurora.Cyan(SHELL_VERSION + LINE_SEP + LINE_SEP))
     } else {
         fmt.Println(aurora.Cyan(SHELL_NAME + " " + SHELL_VERSION))
         fmt.Println()
     }
 
     reader := bufio.NewReader(os.Stdin)
+    loadSession()
     for {
         fmt.Printf("[%s(session: %s): %s]--> ", SHELL_NAME, current_session ,string([]byte(time.Now().String())[:19]))
         str, _ := reader.ReadString('\n')
@@ -85,6 +95,8 @@ func (s *ShellCommand) run () {
         s.runClearCommand()
     case 4:
         s.runLeaveCommand()
+    case 5:
+        s.runSaveCommand()
     case 0: fallthrough
     default:
         s.runRemoteShellCommand()
@@ -166,6 +178,14 @@ func (s *ShellCommand) runRemoteShellCommand() {
     }
 }
 
+func (s *ShellCommand) runSaveCommand() {
+    var sessionString string
+    for k, v := range session {
+        sessionString += k + FIELD_SEP + v + LINE_SEP
+    }
+    ioutil.WriteFile(sessionFile, []byte(sessionString), os.FileMode(int(0777)))
+}
+
 func PrintMsgWithColor(msg string, f *figletlib.Font, maxwidth int, s figletlib.Settings, align string, color colorFunc) {
     lines := figletlib.GetLines(msg, f, maxwidth, s)
     PrintLineWithColor(lines, s.HardBlank(), maxwidth, align, color)
@@ -196,4 +216,16 @@ func PrintLineWithColor(lines []figletlib.FigText, hardblank rune, maxwidth int,
         }
     }
 }
-// TODO  add local file store
+
+
+func loadSession() {
+    if bc, err := ioutil.ReadFile(sessionFile); err == nil {
+        content := string(bc)
+        lines := strings.Split(content, LINE_SEP)
+
+        for _, value := range lines[:len(lines) - 1] {
+            kv := strings.Split(value, FIELD_SEP)
+            session[kv[0]] = kv[1]
+        }
+    }
+}
